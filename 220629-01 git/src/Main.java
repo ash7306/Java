e2a786d2da7720d19722408db341010b14ab4440
Administrator <Administrator@SC-202106161522>
2022-06-29 09:30:42 +0900

    헬로월드 출력 완성

--- 220629-01 git/src/Main.java
+++ 220629-01 git/src/Main.java
@@ -1,4 +1,10 @@
 
 public class Main {
-	
+	public static void main(String[] args) {
+		//헬로월드
+		//commit 변경점을 기록
+		//staged Changes로 변경을 원하는 파일을 끌어다쓰고
+		//커밋 메세지에다가 변경점을 쓰면 됨 그후 커밋
+		System.out.println("헬로월드");
+	}
 }
