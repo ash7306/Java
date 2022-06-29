51b2b28757a6a57f466884dd0421e22ebb408340
Administrator <Administrator@SC-202106161522>
2022-06-29 10:19:49 +0900

    깃허브는 어려워

--- 220629-01 git/src/Main.java
+++ 220629-01 git/src/Main.java
@@ -6,5 +6,9 @@ public class Main {
 		//staged Changes로 변경을 원하는 파일을 끌어다쓰고
 		//커밋 메세지에다가 변경점을 쓰면 됨 그후 커밋
 		System.out.println("헬로월드");
+		System.out.println("깃허브는 어려워");
+		//변경점 보기 
+		//윈도우 =쇼 뷰 = 아더에 history
+		//그럼 변경한 내역이 뜬다
 	}
 }
